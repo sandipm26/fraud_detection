395c185ef4f5ee52292bfd0d2ea2a7234ebaaa35
sandipm26 <sandip.mato@gmail.com>
2023-01-27 23:12:24 -0500

    Added 3 new model code

--- model.py
+++ model.py
@@ -1,3 +1,11 @@
 
 print("Adding all model related code here")
 
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+def model3():
+    print("adding code for ML model 3")
