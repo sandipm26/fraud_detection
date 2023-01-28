447f138344ce5a83f4d04ff26d60e8e6d6f1258d
sandipm26 <sandip.mato@gmail.com>
2023-01-27 23:44:40 -0500

    Added

--- model.py
+++ model.py
@@ -2,12 +2,12 @@
 print("Adding all model related code here")
 
 def model1():
-    print("adding code for ML model 1")
+    print("Adding code for ML Model 1")
 
 def model2():
-    print("adding code for ML model 2")
+    print("Adding code for ML Model 2")
 
 def model3():
-    print("adding code for ML model 3")
+    print("Adding code for ML Model 3")
 
 print("Model 2 is performing best")
